fd7d55dc1db2375740d1c23d64b6555e65ca04bd
WindowsMEMZ <wmemz2333@163.com>
2024-05-03 23:34:28 +0800

    Updated Loc

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CepheusKeyboardKit",
-            resources: [.process("Cepheus.xcstrings")]
+            resources: [.process("Resources/Localizable.xcstrings")]
         ),
     ]
 )
