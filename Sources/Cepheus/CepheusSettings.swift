e87904e60dda182add8efbacfcede43a2b9d8110
ThreeManager785 <tom_20110510@163.com>
2024-07-07 13:56:23 +0800

    Cepheus 2.1.2 incomplete

--- Sources/Cepheus/CepheusSettings.swift
+++ Sources/Cepheus/CepheusSettings.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct CepheusSettingsView: View {
-  public let CepheusVersion = "2.1.0"
+  public let CepheusVersion = "2.1.2"
   public init(CepheusLocalizedByKeyboardLanguage: Bool = true, CepheusRecentUsedEmojiCollectionsAllowed: Bool = true, CepheusSaveWhenDismissed: Bool = false) {
     self.CepheusLocalizedByKeyboardLanguage = CepheusLocalizedByKeyboardLanguage
     self.CepheusRecentUsedEmojiCollectionsAllowed = CepheusRecentUsedEmojiCollectionsAllowed
