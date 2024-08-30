84c7564839d735182477727cc26dfacda9e574d8
Atlantic75 <113956386+ThreeManager785@users.noreply.github.com>
2024-08-31 00:04:21 +0800

    Update CepheusSettings.swift

--- Sources/Cepheus/CepheusSettings.swift
+++ Sources/Cepheus/CepheusSettings.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public struct CepheusSettingsView: View {
-  public let CepheusVersion = "2.4.0 - $1"
+  public let CepheusVersion = "2.4.0"
 //  public init(CepheusLocalizedByKeyboardLanguage: Bool = true, CepheusRecentUsedEmojiCollectionsAllowed: Bool = true, CepheusSaveWhenDismissed: Bool = false) {
 //    self.CepheusLocalizedByKeyboardLanguage = CepheusLocalizedByKeyboardLanguage
 //    self.CepheusRecentUsedEmojiCollectionsAllowed = CepheusRecentUsedEmojiCollectionsAllowed
